af9b05b1a63e341826a3643ef7016993bd5a48b9
Lorenzo <lor.mico@gmail.com>
2020-06-15 14:02:30 +0200

    Aggiunte opzioni, resi print più espliciti

--- pesterer.py
+++ pesterer.py
@@ -2,7 +2,7 @@
 Chiede a Decathlon la disponibilità dei prodotti nei negozi censiti in DB.
 Se trova differenze dall'ultima esecuzione, notifica.
 """
-
+import argparse
 import concurrent.futures
 import json
 import sqlite3
@@ -12,6 +12,7 @@ from dataclasses import dataclass, field
 from typing import Dict, List, Optional
 
 URLFMT = "https://www.decathlon.it/it/ChooseStore_getStoresWithAvailability?storeFullId={storeFullId}&productId={productId}&_={timestamp}"
+
 
 @dataclass
 class Store:
@@ -80,6 +81,17 @@ def create_connection(db_file: str) -> Optional[sqlite3.Connection]:
     return conn
 
 
+def get_store(conn: sqlite3.Connection, store_full_id: str) -> Optional[Store]:
+    cur = conn.cursor()
+    query = """SELECT store_full_id, store_id, store_description FROM stores WHERE store_full_id = ?"""
+    cur.execute(query, (store_full_id,))
+    result = cur.fetchone()
+    if result:
+        return Store(*result)
+    else:
+        return None
+
+
 def get_stores(conn: sqlite3.Connection, only_favs=True) -> List[Store]:
     cur = conn.cursor()
     query = """SELECT store_full_id, store_id, store_description FROM stores"""
@@ -88,6 +100,17 @@ def get_stores(conn: sqlite3.Connection, only_favs=True) -> List[Store]:
     cur.execute(query)
     stores: List[Store] = [Store(*row) for row in cur.fetchall()]
     return stores
+
+
+def get_product(conn: sqlite3.Connection, product_id: str) -> Optional[Product]:
+    cur = conn.cursor()
+    query = """SELECT id, name, color, size FROM products WHERE id = ?"""
+    cur.execute(query, (product_id,))
+    result = cur.fetchone()
+    if result:
+        return Product(*result)
+    else:
+        return None
 
 
 def get_products(conn: sqlite3.Connection, only_favs=True) -> List[Product]:
@@ -113,32 +136,55 @@ def get_product_availability(conn: sqlite3.Connection, product_availability: Pro
 
 
 def update_product_availability(conn: sqlite3.Connection, product_availability: ProductAvailability):
-    print("Aggiornamento della disponibilità del prodotto %s" %
-          product_availability.product_id)
+    product = get_product(conn, product_availability.product_id)
+    assert product is not None
+    store = get_store(conn, "007" + (str(product_availability.store_id).zfill(5)) * 2)
+    assert store is not None
+
+    print("Aggiornamento della disponibilità del prodotto %s %s %s (%d pezz%s presso %s)" % (
+        product.name,
+        product.color,
+        product.size,
+        product_availability.availability,
+        "o" if product_availability.availability == 1 else "i",
+        store.store_description))
     cur = conn.cursor()
-    cur.execute("""UPDATE product_availability SET availability = ? WHERE product_id = ? AND store_id = ?""",
-                (
-                    product_availability.availability,
-                    product_availability.product_id,
-                    product_availability.store_id
-                )
-                )
+    cur.execute("""UPDATE product_availability SET availability = ? WHERE product_id = ? AND store_id = ?""", (
+        product_availability.availability,
+        product_availability.product_id,
+        product_availability.store_id))
 
 
 def insert_product_availability(conn: sqlite3.Connection, product_availability: ProductAvailability):
-    print("Inserimento della disponibilità del prodotto %s precedentemente non censita" %
-          product_availability.product_id)
+    product = get_product(conn, product_availability.product_id)
+    assert product is not None
+    store = get_store(conn, "007" + (str(product_availability.store_id).zfill(5)) * 2)
+    assert store is not None
+
+    print("Inserimento della disponibilità del prodotto %s %s %s (%d pezz%s presso %s) precedentemente non censita" % (
+        product.name,
+        product.color,
+        product.size,
+        product_availability.availability,
+        "o" if product_availability.availability == 1 else "i",
+        store.store_description))
     cur = conn.cursor()
-    cur.execute("""INSERT INTO product_availability (product_id, store_id, availability) VALUES (?,?,?)""",
-                (
-                    product_availability.product_id,
-                    product_availability.store_id,
-                    product_availability.availability
-                )
-                )
+    cur.execute("""INSERT INTO product_availability (product_id, store_id, availability) VALUES (?,?,?)""", (
+        product_availability.product_id,
+        product_availability.store_id,
+        product_availability.availability))
 
 
-def main(only_favs=True):
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-a", "--all", help="elabora tutti i prodotti e negozi, non solo i preferiti",
+                        dest="only_favs", action="store_false")
+    parser.add_argument("-n", "--thread-number", help="numero di thread da usare per le richieste HTTP", 
+                        type=int, default=16)
+    return parser.parse_args()
+
+
+def main(only_favs=True, threads=16):
     conn = create_connection('status.db')
     products: List[Product] = get_products(conn, only_favs)
     stores: List[Store] = get_stores(conn, only_favs)
@@ -146,7 +192,7 @@ def main(only_favs=True):
     print("Creo i thread...")
     product_availabilities: List[ProductAvailability] = list()
     futures: List[concurrent.futures.Future] = list()
-    with concurrent.futures.ThreadPoolExecutor(max_workers=16) as executor:
+    with concurrent.futures.ThreadPoolExecutor(max_workers=threads) as executor:
         for product in products:
             for store in stores:
                 futures.append(executor.submit(
@@ -170,7 +216,5 @@ def main(only_favs=True):
 
 
 if __name__ == "__main__":
-    only_favs = True
-    if len(sys.argv) > 1 and sys.argv[1] == '-a':
-        only_favs = False
-    main(only_favs)
+    args = parse_args()
+    main(args.only_favs, args.thread_number)
